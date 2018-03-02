c716dd3024b1446f364379af0b07910c8544935d
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2018-03-01 16:42:04 -1200

    added navigation bar and some styling

--- src/App.js
+++ src/App.js
@@ -3,19 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(){
+        super()
+
+        this.state = {
+          
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <h1 className="title">Jackson <span className="bold">McGrath</span></h1>
+                    <nav> 
+                        <button>Home</button>
+                        <button>Projects</button>
+                        <button>About Me</button>
+                        <button>Contact Me</button>
+                    </nav>
+                </header>
+                <div className="main">
+                    <p className="brand-statement">
+                        
+                    </p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
